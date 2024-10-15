1ebcbcc706fb1eddd62502262bf85adb89f8fb92
Olawoyin <yustee2017@gmail.com>
2024-10-15 02:03:46 +0100

    correction to con-current file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-"""Async in a loop"""
-
+'''Task 1's module.
+'''
+import asyncio
 from typing import List
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Runs an asynchronous loop within a function
-
-    Arguments:
-        n: Number of times to run loop (type int)
-        max_delay: Delay in secs (type int)
-
-    Returns:
-        list_delay: A list of delays (type float)
-    """
-    list_delay = [await wait_random(max_delay) for x in range(0, n)]
-    list_delay.sort()
-    return list_delay
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
